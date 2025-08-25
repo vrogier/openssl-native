1090703520114f1bbd85561f3bbbe3b79e669042
Vincent Rogier <vince.rogier@gmail.com>
2025-08-25 19:14:57 +0200

    Fixed issue #1 : using now PlatformToolsetVersion macro instead of PlatformToolset

--- nuget/build.cmd
+++ nuget/build.cmd
@@ -1,7 +1,14 @@
 
 echo "Init output folder"
+
+set OUTPUT_FOLDER=.\output
+
+IF EXIST "%OUTPUT_FOLDER%" (
+    rmdir "%OUTPUT_FOLDER%" /s /q
+)
+
 mkdir .\output
-xcopy /y /s /e .\sources\*.* .\output\
+xcopy /y /s /e .\sources\*.* %OUTPUT_FOLDER%
 
 echo "Build OPENSSL"
 call .\build-openssl.cmd
